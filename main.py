04b8b9442de62247e77902596734951e933f2cc6
Flork1s <pipegamboa20041007@gmail.com>
2025-12-05 09:41:01 -0500

    que susto casi que no estoy intentando que se puedan crear jugadores no se ha podido

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI
 from database import create_tables
 from router import jugador
 from contextlib import asynccontextmanager
@@ -8,7 +8,6 @@ async def life(app:FastAPI):
     create_tables()
     print("base de datos funcional.")
     yield
-
-
 app = FastAPI(lifespan=create_tables, title="Sigmotoa FC")
+
 app.include_router(jugador.router, tags=["jugador"], prefix="/jugador")
