4c98005c46b2a9eaea46db16bfa0d1e45e884a8f
Flork1s <pipegamboa20041007@gmail.com>
2025-12-05 08:28:22 -0500

    Se realiza cambio en el models agragendo Jugadordepoprtivo y estadisticas

--- models.py
+++ models.py
@@ -1,14 +1,29 @@
 from typing import Optional, List
 from sqlmodel import SQLModel, Field, Relationship
 
-class Jugador():
+class JugadorPersonal(SQLModel, table=True):
     nombre: str
     numero_cam: int
     year: int
+    image_url: Optional[str] = None
     nacionalidad:str
+
+    partidos: List["Partido"] = Relationship(back_populates="jugador")
+
+class JugadorDeportivo(SQLModel, table=True):
+    altura: float
+    peso: float
+    pie_dominante: str
+    posicion: str
+    year_ingreso_equipo: int
+    estado: str
 
 
 class Estadistica():
+    total_tiempo: int
+    goles_anotados: int
+    faltas: int
+    tarjetas: int
 
 
 
