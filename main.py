1f52dca68c7c3b5313094f9c6ee22af76dbabf06
Sala 3 <sala3@FI-LabSistemas.ucatolica.loc>
2025-12-05 21:27:02 -0500

    se agrego el inicio de la base de datos: el computador no me dejo instalar sqlmodel, lo que yo haga se lo paso y ust confirma los cambios, que pena pana

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from database import create_tables
 from router import jugador
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def life(app:FastAPI):
+    create_tables()
+    print("base de datos funcional.")
+    yield
 app = FastAPI(lifespan=create_tables, title="Sigmotoa FC")
 
 app.include_router(jugador.router, tags=["jugador"], prefix="/jugador")
